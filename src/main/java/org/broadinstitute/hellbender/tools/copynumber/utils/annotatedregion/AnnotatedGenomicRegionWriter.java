014ba3953b3fa29b1a52de5972c6582b40686030
lichtens <lichtens@broadinstitute.org>
2018-01-26 15:12:42 -0500

    Added docs for writer interface.

--- src/main/java/org/broadinstitute/hellbender/tools/copynumber/utils/annotatedregion/AnnotatedGenomicRegionWriter.java
+++ src/main/java/org/broadinstitute/hellbender/tools/copynumber/utils/annotatedregion/AnnotatedGenomicRegionWriter.java
@@ -8,12 +8,12 @@ public interface AnnotatedGenomicRegionWriter extends Closeable {
     /**
      * Write only the header (and any SAMFileHeader or comments)
      *
-     * @param SAMFileHeader
-     * @param comments
-     * @param annotations
-     * @param contigColumnName
-     * @param startColumnName
-     * @param endColumnName
+     * @param SAMFileHeader SAM file header as a multiline string.  {@code null} is allowed, if not available.
+     * @param comments Comments to prepend to the xsv file.  Use an empty list, if no comments are needed.
+     * @param annotations annotation names that do not include the locatable column names.
+     * @param contigColumnName how contig should be rendered
+     * @param startColumnName how start position should be rendered
+     * @param endColumnName how end position should be rendered
      */
     void writeHeader(final String SAMFileHeader, final List<String> comments, final List<String> annotations,
                      final String contigColumnName, final String startColumnName, final String endColumnName);
